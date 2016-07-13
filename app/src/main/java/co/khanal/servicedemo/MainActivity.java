619c5ebeac864b2875ba5f171facbe8362a2ab05
Abhi Khanal <khanal.abhi@gmail.com>
2016-07-12 23:33:03 -0400

    started with intent service.

---
+++ app/src/main/java/co/khanal/servicedemo/MainActivity.java
@@ -0,0 +1,84 @@
+package co.khanal.servicedemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert(fab != null);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Intent intent = new Intent(getApplicationContext(), SleepService.class);
+        intent.putExtra(SleepService.KEY, "Abhi");
+        startService(intent);
+
+        IntentFilter filter = new IntentFilter(ResponseReceiver.KEY);
+        filter.addCategory(Intent.CATEGORY_DEFAULT);
+        ResponseReceiver responseReceiver = new ResponseReceiver();
+        registerReceiver(responseReceiver, filter);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class ResponseReceiver extends BroadcastReceiver{
+
+        public static final String KEY = "co.khanal.intent.action.MESSAGE_PROCESSED";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            TextView data = (TextView)findViewById(R.id.data);
+
+            String msg = intent.getStringExtra(SleepService.KEY);
+            data.setText(msg);
+        }
+    }
+}
---
+++ app/src/main/java/co/khanal/servicedemo/SleepService.java
@@ -0,0 +1,40 @@
+package co.khanal.servicedemo;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import java.util.Locale;
+
+/**
+ * Created by abhi on 7/12/16.
+ */
+public class SleepService extends IntentService {
+
+    public static String KEY = "co.khanal.sleepservice";
+    public static String DATA_KEY = "data_key";
+
+    public SleepService(){
+        super("SleepService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        try {
+            Thread.sleep(4000);
+            String name = intent.getStringExtra(KEY);
+            String message = String.format(Locale.US, "Hello from %s!", name);
+
+            Intent broadcast = new Intent();
+            broadcast.setAction(MainActivity.ResponseReceiver.KEY);
+            broadcast.addCategory(Intent.CATEGORY_DEFAULT);
+            broadcast.putExtra(KEY, message);
+            sendBroadcast(broadcast);
+
+
+
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
